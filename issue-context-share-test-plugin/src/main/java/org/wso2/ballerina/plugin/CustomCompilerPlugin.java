2d0c6722e4969a6b268b0612645fbfca92194ec9
Tharana Wanigaratne <xenocodecs@gmail.com>
2024-01-05 17:00:39 +0530

    Update: Further refactored issue context plugin by removing analysis tasks

--- issue-context-share-test-plugin/src/main/java/org/wso2/ballerina/plugin/CustomCompilerPlugin.java
+++ issue-context-share-test-plugin/src/main/java/org/wso2/ballerina/plugin/CustomCompilerPlugin.java
@@ -1,13 +1,8 @@
 package org.wso2.ballerina.plugin;
 
-import io.ballerina.projects.plugins.AnalysisTask;
-import io.ballerina.projects.plugins.CodeAnalysisContext;
-import io.ballerina.projects.plugins.CodeAnalyzer;
-import io.ballerina.projects.plugins.CompilationAnalysisContext;
 import io.ballerina.projects.plugins.CompilerPlugin;
 import io.ballerina.projects.plugins.CompilerPluginContext;
 import org.wso2.ballerina.ToolAndCompilerPluginConnector;
-import org.wso2.ballerina.internal.ReportLocalIssue;
 
 public class CustomCompilerPlugin extends CompilerPlugin implements ToolAndCompilerPluginConnector {
     String messageFromTool = null;
@@ -21,17 +16,7 @@ public class CustomCompilerPlugin extends CompilerPlugin implements ToolAndCompi
     // Runs during compilation
     @Override
     public void init(CompilerPluginContext pluginContext) {
-        pluginContext.addCodeAnalyzer(new CodeAnalyzer() {
-            @Override
-            public void init(CodeAnalysisContext codeAnalysisContext) {
-                codeAnalysisContext.addCompilationAnalysisTask(new AnalysisTask<CompilationAnalysisContext>() {
-                    @Override
-                    public void perform(CompilationAnalysisContext compilationAnalysisContext) {
-                        System.out.println("Issues context share test plugin engaged!");
-                        System.out.println(messageFromTool);
-                    }
-                });
-            }
-        });
+        System.out.println("Issues context share test plugin engaged!");
+        System.out.println(messageFromTool);
     }
 }
